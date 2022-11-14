09bbec14e617dbf0f8daad750cab062faadc851e
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-11-13 22:39:33 -0800

    Build site intro and about sections

---
+++ src/components/body/Introduction.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container class="intro">
+    <div class="intro__contents d-flex">
+      <div class="intro__contents--wide">
+        <p class="intro__contents--title">
+          CFC-Singles for Christ
+          <br/>Greater Vancouver Area
+        </p>
+        <p class="intro__contents--subtitle mt-5">
+          Communications
+        </p>
+      </div>
+      <div class="intro__contents--narrow">
+        <v-img
+          class="intro__contents--image"
+          :src="logo"
+          max-height="300"
+          max-width="300"
+        />
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default ({
+  name: 'Introduction',
+  
+  data () {
+    return {
+      logo: '/images/intro/website.gif'
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.intro {
+  height: 100vh;
+  padding: 150px 0;
+  line-height: 1.2;
+
+  &__contents {
+    font-size: 50px;
+
+    @media screen and (max-width: 1270px) {
+      flex-direction: column;
+    }
+
+    &--wide {
+      width: 70%;
+
+      @media screen and (max-width: 1270px) {
+        width: 100%;
+      }
+    }
+
+    &--narrow {
+      border-radius: 50%;
+
+      @media screen and (max-width: 1270px) {
+        align-self: end;
+        margin-top: 10px;
+      }
+
+      @media screen and (max-width: 620px) {
+        align-self: center;
+      }
+    }
+
+    &--title {
+      font-size: 30px;
+
+      @media screen and (max-width: 560px) {
+        text-align: center;
+      }
+    }
+
+    &--subtitle {
+      font-size: 80px;
+      font-weight: bold;
+      background: linear-gradient(90deg, #444CE7 30%, #9E77ED 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+
+      @media screen and (max-width: 700px) {
+        font-size: 60px;
+      }
+
+      @media screen and (max-width: 500px) {
+       text-align: center;
+       font-size: 50px;
+      }
+
+      @media screen and (max-width: 400px) {
+       text-align: center;
+       font-size: 35px;
+      }
+    }
+
+    &--image {
+      border-radius: 50%;
+    }
+  }
+}
+</style>
